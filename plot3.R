ddfefca138482b87e2015c54faf06e4a46e06e6d
lm <lm.mix.21@gmail.com>
2015-01-10 20:20:43 +0100

    plot3 update (without melting)

--- plot3.R
+++ plot3.R
@@ -12,9 +12,6 @@ dataPlot <- dataPlot[, 3:6]
 dataPlot$Sub_metering_1 <- as.numeric(as.character(dataPlot$Sub_metering_1))
 dataPlot$Sub_metering_2 <- as.numeric(as.character(dataPlot$Sub_metering_2))
 dataPlot$Sub_metering_3 <- as.numeric(as.character(dataPlot$Sub_metering_3))
-## Reshape data
-library(reshape)
-dataPlot <- melt(dataPlot, id="DateTime")
 ## Open PNG device
 png(filename = "plot3.png", width = 480, height = 480)
 ## Set margins
@@ -22,10 +19,9 @@ par(mar=c(4, 4, 2, 2))
 ## Set x-axis Date format to English
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
 ## Build histogram
-plot(dataPlot$DateTime, dataPlot$value, type="l", xlab="", ylab="Energy sub metering")
-with(subset(dataPlot, variable=="Sub_metering_1"), lines(DateTime, value, col="black"))
-with(subset(dataPlot, variable=="Sub_metering_2"), lines(DateTime, value, col="red"))
-with(subset(dataPlot, variable=="Sub_metering_3"), lines(DateTime, value, col="blue"))
+with(dataPlot, plot(DateTime, Sub_metering_1, col="black", type="l", xlab="", ylab="Energy sub metering"))
+with(dataPlot, lines(DateTime, Sub_metering_2, col="red"))
+with(dataPlot, lines(DateTime, Sub_metering_3, col="blue"))
 legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 ## Close device
 dev.off()
